c4365af9b40613ad7d988aa07a566134b4e9cbc5
indroji-mondal <indro.cse.bu@gmail.com>
2024-10-31 20:57:08 +0600

    Configured ReactRouter, Tailwindcss, DaisyUI

---
+++ src/components/Root/Root.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Root = () => {
+    return (
+        <div>
+            <h1 className='text-xl text-fuchsia-600'>Welcome to the BoiPoka-Book-Vibe</h1>
+        </div>
+    );
+};
+
+export default Root;
