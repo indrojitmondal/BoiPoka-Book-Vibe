373db1208c2689c8816cfce2e495502227d55816
indroji-mondal <indro.cse.bu@gmail.com>
2024-11-01 11:00:24 +0600

    Added loader for bookdetails and used find function

--- src/components/BookDetail/BookDetail.jsx
+++ src/components/BookDetail/BookDetail.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useLoaderData, useParams } from 'react-router-dom';
 
 const BookDetail = () => {
     const {bookId}= useParams();
-    console.log(bookId);
+    console.log(typeof bookId);
+    const id = parseInt(bookId);
+    console.log(typeof id);
+    const data = useLoaderData();
+    console.log(data);
+    const book = data.find( book => book.bookId ===id);
+    console.log(book);
+    const {bookId: currentBookId, image,bookName} = book;
     return (
-        <div>
-            <h2>Book Details: {bookId}</h2>
+        <div className='space-y-3 mb-2'>
+            <h2>Book Details: {currentBookId}</h2>
+            <img className='w-96' src={image} alt={bookName} />
+            <div className='flex gap-5'>
+            <button className="btn btn-active btn-accent">Read </button>
+            <button className="btn btn-active btn-accent">WishList</button>
+            </div>
         </div>
     );
 };
